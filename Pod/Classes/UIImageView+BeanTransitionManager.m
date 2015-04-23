6eaa7af14fee7a5f6fd72a58cefb513d9d71592b
Stephen Walsh <stephenw@donedeal.ie>
2015-04-23 23:09:18 +0100

    Fixed Constraint warning

--- Pod/Classes/UIImageView+BeanTransitionManager.m
+++ Pod/Classes/UIImageView+BeanTransitionManager.m
@@ -26,19 +26,42 @@
 
 - (void)destroyAllConstraintsAndAlignToView:(UIView *)view
 {
+    [self destroyConstraints];
+    [self destroyRelatedParentConstraints];
+    self.frame = view.frame;
+}
+
+- (void)destroyConstraints
+{
     for (NSLayoutConstraint *constraint in self.constraints) {
         if (constraint.firstAttribute == NSLayoutAttributeWidth || constraint.firstAttribute == NSLayoutAttributeHeight) {
             [self removeConstraint:constraint];
         }
     }
-    
-    self.frame = view.frame;
+}
+
+- (void)destroyRelatedParentConstraints
+{
+    for (NSLayoutConstraint *constraint in self.superview.constraints) {
+        if (constraint.firstAttribute == NSLayoutAttributeCenterY
+            && constraint.secondAttribute == NSLayoutAttributeCenterY
+            && constraint.firstItem == self.superview
+            && constraint.secondItem == self) {
+            [self.superview removeConstraint:constraint];
+        }
+        else if (constraint.firstAttribute == NSLayoutAttributeCenterX
+                 && constraint.secondAttribute == NSLayoutAttributeCenterX
+                 && constraint.firstItem == self.superview
+                 && constraint.secondItem == self) {
+            [self.superview removeConstraint:constraint];
+        }
+    }
 }
 
 - (NSArray *)fullscreenConstraintsOnView:(UIView*)view
 {
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
-
+    
     NSLayoutConstraint *widthConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:view attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0];
     NSLayoutConstraint *heightConstraint =[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:view attribute:NSLayoutAttributeHeight multiplier:1.0 constant:0];
     NSLayoutConstraint *constraintX =[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0];
